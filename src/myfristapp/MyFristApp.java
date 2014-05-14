59221f3be8e7a0bad6d45b04acb295884bfb7f42
benzz_st@hotmail.com <>
2014-05-14 09:14:57 +0700

    hi hi

---
+++ src/myfristapp/MyFristApp.java
@@ -0,0 +1,12 @@
+package myfristapp;
+
+public class MyFristApp {
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Metat Muangjiang 55021373");
+        System.out.println("Hello World");
+        
+    }
+    
+}
